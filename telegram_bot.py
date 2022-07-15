9c3c7a01d428b0b52ed189a380b41e8ca47de6a0
astershch <107745329+astershch@users.noreply.github.com>
2022-07-15 23:12:59 +0400

    Publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,11 +8,14 @@ from dotenv import load_dotenv
 def main():
 
     load_dotenv()
-    
+
     bot_token = os.environ['TG_BOT_TOKEN']
     chat_id = os.environ['TG_CHAT_ID']
 
     bot = telegram.Bot(token=bot_token)
+
+    with open('./images/nasa_apod_0..jpg', 'rb') as photo:
+        bot.send_photo(chat_id=chat_id, photo=photo)
 
     bot.send_message(text='Hi', chat_id=chat_id)
 
